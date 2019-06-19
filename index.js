36300c9e2e3386698b0ae8547cf48099b8abb2a7
fnajer <lexactr@gmail.com>
2019-06-19 11:36:11 +0300

    handle getting stories, add nodemon dependency

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const stories = require('./stories');
 
 const app = express();
 
 app.get('/ping', (req, res) => {
   res.send('pong');
 });
+
+app.get('/stories', (req, res) => {
+  res.json(stories);
+});
+
+app.get('/stories/:title', (req, res) => {
+  const { title } = req.params;
+  const filteredStories = stories.filter(
+    story => story.title.includes(title)
+  );
+  res.json(filteredStories);
+})
 
 const PORT = 3000;
 app.listen(PORT, () => {
